728d59a8a05ae884fe9c9c4695d6a7e8ef940f1a
Kevin Benelli <kevinbenelli@Kevins-MacBook-Pro.local>
2021-09-30 12:16:10 -0700

    socket communication is working and is being displayed

--- stocked-client/src/components/chat.js
+++ stocked-client/src/components/chat.js
@@ -1,37 +1,48 @@
 import React from 'react'
-
-import { io } from 'socket.io-client'
 import { useState, useEffect } from 'react'
 
 
 export const Chat = ({socket, username, chatroom}) => {
-    const [message, setMessage] = useState("");
-    
+    const [userMessage, setUserMessage] = useState("");
+    const [messageLog, setMessageLog] = useState([]);
+
+
     const sendMessage = async () => {
       // If user drafts a message then set message for emission
-      if (message !== ""){
+      if (userMessage !== ""){
         // messageData provides more details about message like time
         const messageData = {
-          chatroom: chatroom.toUpperCase(), // stores specific chatroom
+          chatroom: chatroom, // stores specific chatroom
           author: username, // maps message to user name
-          message: message, // sets messsage to message drafted
+          message: userMessage, // sets messsage to message drafted
            // gets time stamp by hours and minutes
-          time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+          time: 
+          new Date(Date.now()).getHours() + 
+          ":" + 
+          new Date(Date.now()).getMinutes() + 
+          ":" + 
+          new Date(Date.now()).getSeconds(),
         };
 
         // Wait for message to be sent before continuing to move forward so make it asyncronous
         // emits message data object to messaging server
-        await socket.emit("send_message", messageData)        
+        await socket.emit("send_message", messageData);   
+        
+        // When we send a message we store our message in the chat log
+        setMessageLog((prevMessageLog) => [...prevMessageLog, messageData])
       }
-    }
+    };
 
     // Listens to whenever there is a change in socket server
     useEffect(() => {
       // listen to receive_message event and create call back function to handle message on client
       // grab data from backend (data)
       socket.on("receive_message", (data) => {
-        
-      })
+        console.log(data);
+        // whenever someone emmit a message it sets message log to what ever it was before plus the new message
+        setMessageLog((prevMessageLog) => [...prevMessageLog, data])
+      });
+      
       
     }, [socket]);
 
@@ -43,7 +54,14 @@ export const Chat = ({socket, username, chatroom}) => {
       </div>
       
       
-      <div className="chat-room-body"> </div>
+      <div className="chat-room-body"> 
+        {
+          // for each element in message list return <h1> message </h1>
+          messageLog.map((messageContent) => {
+              return <h1>{messageContent.message}</h1>
+          })
+        }
+      </div>
       
       
       <div className="chat-room-footer">
@@ -51,7 +69,7 @@ export const Chat = ({socket, username, chatroom}) => {
         type="text" 
         placeholder="Lets Talk Stocks..."
         onChange={(event) => {
-          setMessage(event.target.value)
+          setUserMessage(event.target.value)
         }}
         />
         <button onClick={sendMessage}>&#9658;</button>
--- stocked-client/src/components/socketclient.js
+++ stocked-client/src/components/socketclient.js
@@ -3,45 +3,52 @@ import React from 'react'
 import { io } from 'socket.io-client'
 import { useState } from 'react'
 import { Chat } from './chat'
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "http://localhost:5001";
 const socket = io(ENDPOINT);
 
 export const SocketClient = () => {
   const [userName, setUsername] = useState("");
   const [chatRoom, setChatRoom] = useState("");
+  const [showChatBox, setShowChatBox] = useState(false);
+
 
   // function emit socket event to join room. 
   const joinRoom = () => {
     // GET HASHSET AND CHECK IF CHATROOM TICKER EXISTS IN TICKER SET; ELSE ERROR MESSAGE
     if(userName !== "" && chatRoom !== ""){
-      socket.emit("join_chat_room", chatRoom.toUpperCase());
+      socket.emit("join_chat_room", chatRoom);
+      setShowChatBox(true);
     }
   };
 
   return (
     <div className="center">
-      <div>
-      <h3> Join Stocked Chat Room Now! </h3>
-        <input 
-        type="text" 
-        placeholder="Enter Name" 
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}/>
-        <input 
-        type="text" 
-        placeholder="Enter Ticker" 
-        onChange={(event) => {
-          setChatRoom(event.target.value);
+      {/* If chat box is not displayed show enter chat room; else show chat box */}
+      {!showChatBox ? (
+        <div>
+          <h3> Join Stocked Chat Room Now! </h3>
+            <input 
+            type="text" 
+            placeholder="Enter Name" 
+            onChange={(event) => {
+              setUsername(event.target.value.toUpperCase());
+            }}/>
+            <input 
+            type="text" 
+            placeholder="Enter Ticker" 
+            onChange={(event) => {
+              setChatRoom(event.target.value.toUpperCase());
+            
+            }}/>
+            
+            <button onClick={joinRoom}> Join Chat Room </button>        
+        </div>
+      ) : (
+        // Call our chat component and pass in the socket
+        // Keep track of the username and chat room being used so we pass that in as pops
+          <Chat socket={socket} username={userName} chatroom={chatRoom}/>
+      )}
         
-        }}/>
-        
-        <button onClick={joinRoom}> Join Chat Room </button>        
-
-        {/* We call our chat component and pass in the socket
-        We also want to keep track of the username and chat room being used so we pass that in as pops */}
-        <Chat socket={socket} username={userName} chatroom={chatRoom}/>
-      </div>
     </div>
-  )
+  );
 } 
