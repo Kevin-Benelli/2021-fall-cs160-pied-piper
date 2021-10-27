e58f67b9c92b12c4492187429fac491ae5d5876d
trinityd <ftrinitydickinson@gmail.com>
2021-10-26 18:47:05 -0700

    Chart improvements
    - Add chart setting controls
    - Make time display dependent on resolution
    - Better documentation

--- stocked-client/src/components/chart.js
+++ stocked-client/src/components/chart.js
@@ -2,8 +2,13 @@ import './chart.css';
 import loadingGif from "../pics/loading.gif";
 import React, { useRef, useEffect, useState } from "react";
 import axios from 'axios';
+import { Button, Label, Popover } from '@blueprintjs/core';
+import { DatePicker, TimePrecision } from '@blueprintjs/datetime';
+import { Popover2 } from '@blueprintjs/popover2';
 
 // Helpers
+
+// Date stuff
 function deltaDate(input, days, months, years) {
   return new Date(
     input.getFullYear() + years,
@@ -18,6 +23,13 @@ function deltaDate(input, days, months, years) {
     )
   );
 }
+let dateForm = new Intl.DateTimeFormat("en", {
+  timeStyle: "short",
+  hour12: false,
+  dateStyle: "short"
+});
+
+// Range stuff
 // from and to are 2 number arrays, s is the number to map
 function mapRange(from, to, s) {
   return to[0] + ((s - from[0]) * (to[1] - to[0])) / (from[1] - from[0]);
@@ -30,20 +42,28 @@ function isInRangeExclusive(x, range) {
   return x > range[0] && x < range[1];
 }
 
+// Stock chart component
 export const Chart = (props) => {
+  // Chart data from Finnhub
   let [chartData, setChartData] = useState(null);
+  // Error msg
   let [error, setError] = useState(null);
+  // Loaded state, used to show loading gif
   let [loaded, setLoaded] = useState(false);
   let width = "800px";
   let height = "500px";
-  let resolution = "30";
+  // Candle resolution - Either "1", "5", "15", "30", "60", "D", "W", or "M"
+  let [resolution, setResolution] = useState("30");
   let symbol = props.ticker.toUpperCase();
 
+  // Start 1 day ago, end today by default.
   let now = new Date();
   let then = deltaDate(now, -1, 0, 0);
-  let from = Math.floor(then.getTime() / 1000);
-  let to = Math.floor(now.getTime() / 1000);
+  let [from, setFrom] = useState(Math.floor(then.getTime() / 1000)); // Need to / 1000 or * 1000 throughout to account for javascript Unix time being in milliseconds, but Finnhub using seconds.  
+  let [to, setTo] = useState(Math.floor(now.getTime() / 1000));
 
+  // Call backend to get chart data
+  // TODO: Call backend to get setting defaults
   useEffect(() => {
     axios.get("http://localhost:5001/chart_data", { 
       params: {
@@ -53,16 +73,26 @@ export const Chart = (props) => {
         to: to
       } 
     }).then((response) => {
-      // console.log("Got response")
-      // console.log(response)
       setChartData(response.data.chartData);
       setError(response.data.error);
       setLoaded(true);
     })
-  }, []);
+  }, [resolution, symbol, from, to]);
+
+  // Callbacks for datetime pickers
+  let handleFromChange = (date) => {
+    setFrom(Math.floor(date.getTime() / 1000));
+  }
+  let handleToChange = (date) => {
+    setTo(Math.floor(date.getTime() / 1000));
+  }
+  let timePickerProps = {
+    showArrowButtons: true,
+    useAmPm: false,
+  }
 
   return (
-    <div className="center">
+    <div className="chartArea center">
       <div className="chart">
         <StockChart
           chartData={chartData}
@@ -76,6 +106,56 @@ export const Chart = (props) => {
           to={to}
         />
       </div>
+      { /* If we're done loading, and there's no error, display the chart settings */ }
+      {loaded && error === null && (
+        <div className="chartSettings">
+          { /* Inputs that change resolution, from, and to dates */ }
+          <Label>
+            From:
+            <Popover2 placement="top"
+                      content={
+                        <DatePicker className="chart-from-datepicker"
+                                    onChange={handleFromChange}
+                                    timePickerProps={timePickerProps}
+                                    timePrecision={TimePrecision.SECOND}
+                                    />
+                      }>
+                <Button text={ dateForm.format(from * 1000) }/>
+            </Popover2>
+          </Label>
+          <Label>
+            To:
+            <Popover2 placement="top"
+                      content={
+                        <DatePicker className="chart-from-datepicker"
+                                    onChange={handleToChange}
+                                    timePickerProps={timePickerProps}
+                                    timePrecision={TimePrecision.SECOND}
+                                    />
+                      }>
+                <Button text={ dateForm.format(to * 1000) }/>
+            </Popover2>
+          </Label>
+          <Label>
+            Resolution: { /* Either "1", "5", "15", "30", "60", "D", "W", "M" */ }
+            <div className="bp3-html-select chart-resolution">
+              <select className="chart-resolution-select"
+                      onChange={(e) => setResolution(e.target.value)}
+                      >
+                <option value="1">1</option>
+                <option value="5">5</option>
+                <option value="15">15</option>
+                <option value="30" selected>30</option>
+                <option value="60">60</option>
+                <option value="D">D</option>
+                <option value="W">W</option>
+                <option value="M">M</option>
+              </select>
+              <span className="bp3-icon bp3-icon-double-caret-vertical"></span>
+            </div>
+          </Label>
+        </div>
+      )}
     </div>
   );
 }
@@ -84,12 +164,15 @@ class StockChart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      zoomLevel: 1,
+      zoomLevel: 1, // TODO
       mouseX: null,
       mouseY: null
     };
   }
 
+  // Called every frame by the underlying canvas element
+  // Something drawn later in the draw loop will be on top of things drawn before it - like a real painting 
+  // So order matters (when we care about things being drawn on top of other things)
   draw = (ctx, frameCount) => {
     // Color palette
     let white = "#ffffff";
@@ -114,12 +197,12 @@ class StockChart extends React.Component {
     let timeAreaHeight = 40;
     let pricesAreaHeight = height - timeAreaHeight;
 
-    // Background
+    // Draw Background
     ctx.clearRect(0, 0, width, height);
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, width, height);
 
-    // Prices Area
+    // Draw Prices Area
     let pricesAreaPos = width - pricesAreaWidth;
     let pricesAreaSeparatorWidth = 2;
     ctx.fillStyle = pricesAreaSeparatorColor;
@@ -135,19 +218,20 @@ class StockChart extends React.Component {
     let topPriceHeight, botPriceHeight;
     let lastHeight = 0;
     ctx.textAlign = "center";
+    // Loop thru prices and draw them 
     for (let i = 0; i < prices.length; i++) {
       let price = prices[i];
       let x = pricesAreaPos + pricesAreaWidth / 2;
       let y = (pricesAreaHeight / prices.length) * i + fontSize + topPadding;
       if (i === 0) topPriceHeight = y;
       else if (i === prices.length - 1) botPriceHeight = y;
-      if (Math.abs(y - lastHeight) >= fontSize + pricePadding) {
+      if (Math.abs(y - lastHeight) >= fontSize + pricePadding) { // Only draw if we won't overlap
         ctx.fillText(price.toFixed(2), x, y);
         lastHeight = y;
       }
     }
 
-    // Candles and Time Area
+    // Candles Area
     let candles = this.props.chartData.candles;
     let amtOfCandles = candles["c"].length;
     let rightPadding = 40;
@@ -155,8 +239,9 @@ class StockChart extends React.Component {
     let candleWidth = (candlesAndTimeAreaWidth - rightPadding) / amtOfCandles;
     let stickWidth = Math.max(candleWidth / 20, 1);
     let lastPos = 0;
-    let minTimeWidth = 50;
+    let minTimeWidth = 50; // Minimum width of drawn time in pixels
     let candleInfo = [];
+    // Loop thru candles
     for (let i = 0; i < amtOfCandles; i++) {
       let open = candles["o"][i];
       let close = candles["c"][i];
@@ -165,6 +250,7 @@ class StockChart extends React.Component {
       let timestamp = candles["t"][i];
       let volume = candles["v"][i];
 
+      // Calculate pixel heights, relative to canvas, of open, close, high, and low prices
       let openHeight = this.getHeightOfPrice(
         open,
         topPriceHeight,
@@ -186,6 +272,7 @@ class StockChart extends React.Component {
         botPriceHeight
       );
 
+      // Change color based on price moving up, down, or flat thru the candle range
       let candleColor;
       if (open < close) {
         candleColor = posCandleColor;
@@ -199,14 +286,17 @@ class StockChart extends React.Component {
       let candlePos = i * candleWidth + leftRightMargin;
       let actCandleWidth = candleWidth - leftRightMargin;
       let stickPos = candlePos + actCandleWidth / 2 - stickWidth / 2;
+      // Draw the candle
       ctx.fillRect(
         candlePos,
         Math.min(openHeight, closeHeight),
         actCandleWidth,
         Math.abs(openHeight - closeHeight)
       );
+      // Draw the stick in the middle of the candle
       ctx.fillRect(stickPos, highHeight, stickWidth, lowHeight - highHeight);
 
+      // Push given/calculated info about the candle to an array so we don't have to recalculate it later and its all in one place - not used for much right now besides mouse hovering
       candleInfo.push({
         timestamp: timestamp,
         close: close,
@@ -217,47 +307,62 @@ class StockChart extends React.Component {
         highHeight: highHeight,
         low: low,
         lowHeight: lowHeight,
-        xRange: [candlePos, candlePos + actCandleWidth]
+        xRange: [candlePos, candlePos + actCandleWidth],
+        middlePos: stickPos,
       });
 
-      // Time
-      let increment = 5;
+      // Time Area
       let date = new Date(timestamp * 1000);
-      if (
-        date.getMinutes() % increment === 0 &&
-        Math.abs(stickPos - lastPos) >= minTimeWidth
-      ) {
-        ctx.fillStyle = timeTextColor;
-        let strDate;
-        if (date.getDate() === 0) {
-          strDate = date.toLocaleDateString(undefined, { month: "short" });
-        } else {
-          strDate = date.toTimeString().slice(0, 5);
+      // Draw the time differently based on resolution
+      if(["1", "5", "15", "30", "60"].includes(this.props.resolution)) { // Hour/minute
+        let increment = 5;
+        if (
+          date.getMinutes() % increment === 0 &&
+          Math.abs(stickPos - lastPos) >= minTimeWidth // Only draw if we won't overlap
+        ) {
+          ctx.fillStyle = timeTextColor;
+          let strDate;
+          if (date.getDate() === 0) {
+            strDate = date.toLocaleDateString(undefined, { month: "short" });
+          } else {
+            strDate = date.toTimeString().slice(0, 5);
+          }
+          ctx.fillText(strDate, stickPos, height - timeAreaHeight / 2); // Draw time underneath the candlestick
+          lastPos = stickPos; // Track last x position to avoid overlapping
         }
-        ctx.fillText(strDate, stickPos, height - timeAreaHeight / 2);
-        lastPos = stickPos;
+      } else {
+        if (Math.abs(stickPos - lastPos) >= minTimeWidth) { // Only draw if we won't overlap
+          ctx.fillStyle = timeTextColor;
+          let strDate;
+          if (date.getDate() === 0) {
+            strDate = date.toLocaleDateString(undefined, { month: "short" });
+          } else {
+            if(["D", "W"].includes(this.props.resolution)) { // Day/month
+              strDate = date.toLocaleDateString('en-US', { day: 'numeric', month: 'numeric' });
+            } else if("M" === this.props.resolution) { // Month/year
+              strDate = date.toLocaleDateString('en-US', { month: 'short', year: "2-digit" });
+            }
+          }
+          ctx.fillText(strDate, stickPos, height - timeAreaHeight / 2); // Draw time underneath the candlestick
+          lastPos = stickPos; // Track last x position to avoid overlapping
+        }
       }
     }
 
-    let dateForm = new Intl.DateTimeFormat("en", {
-      timeStyle: "short",
-      hour12: false,
-      dateStyle: "short"
-    });
-
-    // Mouse
+    // Interactive Bits
+    // Mouse hover crosshair
     let { mouseX, mouseY } = this.state;
-    if (mouseX && mouseY) {
+    if (mouseX && mouseY) { // If we're hovering over the canvas
+      // Draw dashed crosshair centered at mouse
       ctx.setLineDash([8, 6]);
       ctx.strokeStyle = mouseLinesColor;
-
       ctx.beginPath();
       ctx.moveTo(mouseX, mouseY);
       ctx.lineTo(0, mouseY);
       ctx.stroke();
       ctx.beginPath();
       ctx.moveTo(mouseX, mouseY);
-      ctx.lineTo(candlesAndTimeAreaWidth, mouseY);
+      ctx.lineTo(candlesAndTimeAreaWidth, mouseY); // Doesn't go into prices area
       ctx.stroke();
       ctx.beginPath();
       ctx.moveTo(mouseX, mouseY);
@@ -268,11 +373,21 @@ class StockChart extends React.Component {
       ctx.lineTo(mouseX, height);
       ctx.stroke();
 
-      ctx.setLineDash([]);
-      let hoverCandle = candleInfo.find(({ xRange }) => {
-        return isInRangeInclusive(mouseX, xRange);
-      });
-      if (hoverCandle) {
+      ctx.setLineDash([]); // Stop drawing dashed lines
+
+      // Find closest candle to mouse
+      let minimumDistance = Number.MAX_SAFE_INTEGER;
+      let hoverCandle = null;
+      for(let candle of candleInfo) {
+        let distance = Math.abs(mouseX - candle.middlePos);
+        if (distance < minimumDistance) {
+          minimumDistance = distance;
+          hoverCandle = candle;
+        }
+      }
+
+      // Draw info about the candle underneath the crosshair
+      if (hoverCandle) { // If we're hovering over a candle
         let { timestamp, open, close, low, high } = hoverCandle;
         let mouseDate = new Date(timestamp * 1000);
         ctx.fillStyle = "green";
@@ -294,7 +409,7 @@ class StockChart extends React.Component {
       }
     }
 
-    // Info
+    // Draw info about current ticker and settings
     ctx.fillStyle = infoTextColor;
     ctx.textAlign = "left";
     let infoFrom = new Date(this.props.from * 1000);
@@ -310,17 +425,23 @@ class StockChart extends React.Component {
     ctx.fillText(infoStr, 5, fontSize + 5);
   };
 
+  // Convert price to height, given the top and bottom price height (relative to the canvas)
   getHeightOfPrice = (price, topPriceHeight, botPriceHeight) => {
     let { max, min } = this.props.chartData.computed;
+    // Given two ranges: min price-max price, and bottom height-top height, where would a given price be? Map one range to another
     return mapRange([min, max], [botPriceHeight, topPriceHeight], price);
   };
 
+  // Generate an array of displayed price values for the chart
   getDisplayedPrices = () => {
     let { max, min } = this.props.chartData.computed;
-    let zoomLevel = this.state.zoomLevel;
+    // let zoomLevel = this.state.zoomLevel; // TODO
+    let zoomLevel = 1;
     let defaultStep = 1;
 
     let prices = [];
+    // Start at max price, go down step by step until we hit min price
+    // Will always include max and min
     for (
       let curr = Math.ceil(max);
       curr > min;
@@ -332,6 +453,7 @@ class StockChart extends React.Component {
     return prices;
   };
 
+  // Keep track of mouse position
   updateMousePos = (canvasX, canvasY) => {
     this.setState({
       mouseX: canvasX,
@@ -345,6 +467,7 @@ class StockChart extends React.Component {
     return (
       <div className="center">
         <div className="chart" style={{ width: width, height: height }}>
+        { /* If we're done loading, and there's no error, display the canvas */ }
           {loaded && error === null && (
             <Canvas
               draw={this.draw}
@@ -358,11 +481,18 @@ class StockChart extends React.Component {
               updateMousePos={this.updateMousePos}
             />
           )}
+          { /* If we're done loading, but there's an error, display the error */ }
           {loaded && error !== null && (
-            <div> 
-              Error 404: ticker not found
-            </div>
+            <React.Fragment> { /* React.Fragment is just a way to wrap multiple child components without adding another actual component onto the rendered page. When doing conditional rendering like this you can only have one component in the ( ) section */ }
+              <div> 
+                Error 404: ticker not found
+              </div>
+              <div> 
+                More info: {error}
+              </div>
+            </React.Fragment>
           )}
+          { /* If we're  still loading, show a loading spinner */ }
           {!loaded && <img src={loadingGif} alt="Loading..." />}
         </div>
       </div>
