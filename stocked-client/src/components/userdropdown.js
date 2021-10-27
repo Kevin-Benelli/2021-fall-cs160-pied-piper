ee9329522d66502a9d5303fbc0ed37d2ff2b2d96
EdwardSridhar <edwardwaltham@gmail.com>
2021-10-27 14:33:05 -0700

    User dropdown menu with settings, home, chat, and mystery button. NEEDS CSS IMPROVEMENTS (#13)
    Co-authored-by: Edward <edward.sridhar@sjsu.edu>

--- stocked-client/src/components/userdropdown.js
+++ stocked-client/src/components/userdropdown.js
@@ -3,6 +3,11 @@ import { Button, Intent } from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
 import './userdropdown.css';
 import { clearLocalStorageItem } from 'utils/storage';
+import {
+  HashRouter,
+  Route,
+  NavLink
+} from "react-router-dom";
 
 export const UserDropdown = (props) => {
   let { loggedInUsername, setLoggedInUsername } = props;
@@ -11,11 +16,34 @@ export const UserDropdown = (props) => {
     setLoggedInUsername(null);
   }
 
+
   let dropdown = (
     <div className="userdropdown center">
-      <Button className="logout"
+
+      <HashRouter basename="/">
+
+      { /* Creates the settings button and goes back home */ }
+      <NavLink to = {'/'}>
+        <Button>Home</Button><br/>
+      </NavLink>
+
+      { /* Creates the settings button and goes to the settings page */ }
+      <NavLink to = {'/settings'}>
+        <Button>Settings</Button><br/>
+      </NavLink>
+
+      { /* Creates the chat button and goes to the chat page */ }
+      <NavLink to = {'/chat'}>
+        <Button>Chat</Button><br/> 
+      </NavLink>
+
+      <Button className="mystery" // ???
+                onClick={ event => window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" }> FREE BITCOIN CLICK HERE!! </Button><br/>
+
+      <Button className="logout" // Creates the logout button
                 onClick={ logout }> Logout </Button><br/>
       To be continued
+      </HashRouter>
     </div>
   );
 
