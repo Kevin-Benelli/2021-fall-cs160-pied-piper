428c39c7b87459530fec8a839a5e1f88e4dd91c9
Kevin Benelli <kevinbenelli@Kevins-MacBook-Pro.local>
2021-11-15 13:49:08 -0800

    created test case for entering chat session (w/ tsla)

--- stocked-testing/test-chat.py
+++ stocked-testing/test-chat.py
@@ -4,19 +4,22 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC, wait
 from selenium.webdriver.common.action_chains import ActionChains 
+import time
 
 chat_header_locator = "//h3[contains(text(), 'Join Stocked Chat Room Now!')]"
 chat_join_room_name_locator = "//input[contains(@placeholder, 'Enter Name')]"
 chat_join_room_ticker_locator = "//input[contains(@placeholder, 'Enter Ticker')]"
 chat_join_room_button_locator = "//button[contains(@class, 'joinChatRoomButton')]"
+chat_tesla_session_header_text_locator = "//p['TSLA']"
 
 def verify_chat_header_text():
     try:
         chat_header_field = driver.find_element(By.XPATH, chat_header_locator)
         print(chat_header_field.text)
         return chat_header_field.text
+
     except NoSuchElementException:
         return False
 
@@ -24,6 +27,7 @@ def verify_chat_join_room_fields_exist():
     try:
         driver.find_element(By.XPATH, chat_join_room_name_locator)
         driver.find_element(By.XPATH, chat_join_room_ticker_locator)
+
     except NoSuchElementException:
         return False
     
@@ -36,12 +40,28 @@ def verify_chat_join_room_button_text():
         join_room_button_text = driver.find_element(By.XPATH, chat_join_room_button_locator)
         print(join_room_button_text.text)
         return join_room_button_text.text
+
     except NoSuchElementException:
         return False
     
 
 def verify_open_chat():
-    pass    
+    try:
+        
+        join_room_button_text = driver.find_element(By.XPATH, chat_join_room_button_locator) 
+        name_field_elem = driver.find_element(By.XPATH, chat_join_room_name_locator)
+        ticker_field_elem = driver.find_element(By.XPATH, chat_join_room_ticker_locator)
+
+        name_field_elem.send_keys("Kbenelli")
+        ticker_field_elem.send_keys("TSLA")
+        join_room_button_text.click()
+        
+        chat_tesla_session_header_text_elem = driver.find_element(By.XPATH, chat_tesla_session_header_text_locator)
+        print(chat_tesla_session_header_text_elem.text)
+        return chat_tesla_session_header_text_elem.text
+        
+    except NoSuchElementException:
+        return False
 
 def main():
  
@@ -57,7 +77,7 @@ def main():
     expected_chat_header_text = "Join Stocked Chat Room Now!"
     expected_chat_join_room_field_placeholder_text = True
     expected_chat_button_text = "Join Chat Room"
-
+    expected_open_chat_session_header_text = "Live Chat Room for TSLA"
 
     assert verify_chat_header_text() == expected_chat_header_text # verifies if chat header text is correct
     
@@ -65,6 +85,7 @@ def main():
     
     assert verify_chat_join_room_button_text() == expected_chat_button_text # verifies button and button text is correct
     
+    assert verify_open_chat() == expected_open_chat_session_header_text
     
     driver.close() # closes webdriver after test completion 
 
