195aca6dfbd762ef779673badde32c14654ce782
trinityd <ftrinitydickinson@gmail.com>
2021-11-16 17:48:25 -0800

    Move checks to functions, add asserts, cleaned unused imports, added comment about -1 index

--- stocked-testing/stocked-chart-test.py
+++ stocked-testing/stocked-chart-test.py
@@ -15,11 +15,67 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains 
 
-# import pandas as pd
-# from pandas import ExcelWriter
-# from pandas import ExcelFile
-# from openpyxl import load_workbook
-# import numpy as numpy
+def verify_chart_exists():
+    try:
+        chart = driver.find_element(By.CSS_SELECTOR, ".chart")
+        return True
+    except NoSuchElementException:
+        return False
+
+def verify_from_datetime_selector():
+    try:
+        secondsToWaitIfElementIsNotFound = 3
+        chart_from_button = WebDriverWait(driver, secondsToWaitIfElementIsNotFound).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".chart-from-button")))
+        chart_from_button.click()
+        from_date = driver.find_element(By.CSS_SELECTOR, ".DayPicker-Day")
+        from_date.click()
+        driver.implicitly_wait(secondsToWaitIfElementIsNotFound) # Allow time for a new month to load if needed
+        from_hour_picker = driver.find_element(By.CSS_SELECTOR, ".bp3-timepicker-input.bp3-timepicker-hour")
+        from_hour_picker.click()
+        from_hour_picker.send_keys(Keys.ARROW_RIGHT)
+        from_hour_picker.send_keys(Keys.BACK_SPACE)
+        from_hour_picker.send_keys("9")
+
+    except NoSuchElementException:
+        return False
+
+    return verify_chart_exists()
+
+def verify_to_datetime_selector():
+    try:
+        secondsToWaitIfElementIsNotFound = 30
+        chart_to_button = driver.find_element(By.CSS_SELECTOR, ".chart-to-button")
+        chart_to_button.click()
+        to_dates = driver.find_elements(By.CSS_SELECTOR, ".DayPicker-Day")
+        to_dates[-1].click() # Python way to index last element in a list
+        driver.implicitly_wait(secondsToWaitIfElementIsNotFound) # Allow time for a new month to load if needed
+        try:
+          to_hour_picker = driver.find_element(By.CSS_SELECTOR, ".bp3-timepicker-input.bp3-timepicker-hour")
+          to_hour_picker.click()
+        except:
+          to_hour_picker = driver.find_element(By.CSS_SELECTOR, ".bp3-timepicker-input.bp3-timepicker-hour")
+          to_hour_picker.click()
+        to_hour_picker.send_keys(Keys.ARROW_RIGHT)
+        to_hour_picker.send_keys(Keys.BACK_SPACE)
+        to_hour_picker.send_keys("17")
+
+    except NoSuchElementException:
+        return False
+
+    return verify_chart_exists()
+
+def verify_resolution_picker():
+    try:
+      resolution_picker = driver.find_element(By.CSS_SELECTOR, ".chart-resolution-select")
+      resolution_picker.click()
+      resolution_option = driver.find_element(By.CSS_SELECTOR, ".chart-resolution-select option[value='60']")
+      resolution_option.click()
+
+    except NoSuchElementException:
+        return False
+
+    return verify_chart_exists()
 
 def main():
  
@@ -30,44 +86,18 @@ def main():
     driver = webdriver.Chrome()
     driver.get(url)    
    
-    secondsToWaitIfElementIsNotFound = 3
-
+    assert verify_chart_exists() == True
+    
     # Test opening from and to datetime selectors and making a selection
-    # Expected result: first date to the last date in the date picker popups selected, 9:00 AM to 5:00 PM
-    chart_from_button = WebDriverWait(driver, secondsToWaitIfElementIsNotFound).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, ".chart-from-button")))
-    chart_from_button.click()
-    from_date = driver.find_element(By.CSS_SELECTOR, ".DayPicker-Day")
-    from_date.click()
-    driver.implicitly_wait(secondsToWaitIfElementIsNotFound) # Allow time for a new month to load if needed
-    from_hour_picker = driver.find_element(By.CSS_SELECTOR, ".bp3-timepicker-input.bp3-timepicker-hour")
-    from_hour_picker.click()
-    from_hour_picker.send_keys(Keys.ARROW_RIGHT)
-    from_hour_picker.send_keys(Keys.BACK_SPACE)
-    from_hour_picker.send_keys("9")
-
-    chart_to_button = driver.find_element(By.CSS_SELECTOR, ".chart-to-button")
-    chart_to_button.click()
-    to_dates = driver.find_elements(By.CSS_SELECTOR, ".DayPicker-Day")
-    to_dates[-1].click()
-    driver.implicitly_wait(secondsToWaitIfElementIsNotFound) # Allow time for a new month to load if needed
-    try:
-      to_hour_picker = driver.find_element(By.CSS_SELECTOR, ".bp3-timepicker-input.bp3-timepicker-hour")
-      to_hour_picker.click()
-    except:
-      to_hour_picker = driver.find_element(By.CSS_SELECTOR, ".bp3-timepicker-input.bp3-timepicker-hour")
-      to_hour_picker.click()
-    to_hour_picker.send_keys(Keys.ARROW_RIGHT)
-    to_hour_picker.send_keys(Keys.BACK_SPACE)
-    to_hour_picker.send_keys("17")
+    # Expected result: first date to the last date in the date picker popups selected, 9:00 AM to 5:00 PM. Chart still exists.
+    assert verify_from_datetime_selector() == True
+    assert verify_to_datetime_selector() == True
 
     # Test setting the resolution
     # Expected result: resolution is set to 60 minutes
-    resolution_picker = driver.find_element(By.CSS_SELECTOR, ".chart-resolution-select")
-    resolution_picker.click()
-    resolution_option = driver.find_element(By.CSS_SELECTOR, ".chart-resolution-select option[value='60']")
-    resolution_option.click()
+    assert verify_resolution_picker() == True
 
+    # Click on the chart to get out of resolution picker
     chart = driver.find_element(By.CSS_SELECTOR, ".chart")
     chart.click()
 
